730ae3b3f39dbee67e36ff9220ce4fb14816a6c5
Luuk Veenis <luukveenis@shaw.ca>
2014-09-16 19:22:03 -0700

    Minor refactoring on steps

--- lib/transit/step.rb
+++ lib/transit/step.rb
@@ -2,7 +2,7 @@ module Transit
   class Step
 
     def initialize params
-      @instructions = params['html_instructions'].encode('ASCII', encode_options) if params[:html_instructions]
+      @instructions = params['html_instructions']
       @mode = params['travel_mode']
       @transit_details = params['transit_details'] if transit?
       @sub_steps = parse_sub_steps params['steps']
@@ -24,10 +24,15 @@ module Transit
       @mode == 'TRANSIT'
     end
 
+    def walking?
+      @mode == 'WALKING'
+    end
+
+
     def display
-      if @mode == 'WALKING'
+      if walking?
         puts "Not yet implemented"
-      elsif @mode == 'TRANSIT'
+      elsif transit?
         puts ""
         puts "Depart: #{@transit_details['departure_time']['text']}"
         puts "Arrive: #{@transit_details['arrival_time']['text']}"
